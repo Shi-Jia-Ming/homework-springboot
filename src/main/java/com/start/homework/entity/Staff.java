d027612c279d22ed0cc44ec373ef37f1e77089e5
Shi-Jia-Ming <2516844846@qq.com>
2024-04-27 00:59:39 +0800

    Add entity class.

--- src/main/java/com/start/homework/entity/Staff.java
+++ src/main/java/com/start/homework/entity/Staff.java
@@ -1,5 +1,11 @@
 package com.start.homework.entity;
 
+import java.sql.Date;
+
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
+
+import com.start.homework.utils.Gender;
+
 import lombok.Data;
 
 @Data
@@ -13,12 +19,17 @@ public class Staff {
     // 姓名
     private String name;
     // 性别
-    private int gender;
+    private Gender gender;
     // 图像
     private String image;
     // 职务
-//    private int
-
+    private Job job;
+    // 入职时间
+    private Date entryDate;
+    // 所属的部门
+    private Department department;
+    // 创建时间
+    private Date createAt;
+    // 更新时间
+    private Date updateAt;
 }
-
-
