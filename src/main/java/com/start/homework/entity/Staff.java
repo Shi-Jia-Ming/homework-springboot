bcb2ff0699e9ecd01593bc8ff6535e6fba0712b9
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-04-27 00:16:37 +0800

    develop: Migrate.

---
+++ src/main/java/com/start/homework/entity/Staff.java
@@ -0,0 +1,24 @@
+package com.start.homework.entity;
+
+import lombok.Data;
+
+@Data
+public class Staff {
+    // 员工 id
+    private int id;
+    // 用户名
+    private String username;
+    // 密码
+    private String password;
+    // 姓名
+    private String name;
+    // 性别
+    private int gender;
+    // 图像
+    private String image;
+    // 职务
+//    private int
+
+}
+
+
---
+++ src/main/java/com/start/homework/utils/Gender.java
@@ -0,0 +1,4 @@
+package com.start.homework.utils;
+
+public enum Gender {
+}
